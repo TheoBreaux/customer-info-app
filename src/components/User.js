8be91419d1a285c5405d54fd7ff7ed4417f77c79
Theo Breaux <theobreaux@gmail.com>
2023-05-28 20:41:17 -0700

    corrections

--- src/components/User.js
+++ src/components/User.js
@@ -4,17 +4,9 @@ import "./User.css";
 
 const User = (props) => {
   return (
-    //     <Card className="card">
-    //       {props.userInfo.map((user) => (
-    //         <div className="user-information">{`${user.userName} (${user.userAge} years old)`}</div>
-    //       ))}
-    //     </Card>
-    //   );
-    // };
-
     <div>
       {props.userInfo.map((user, index) => (
-        <Card key={index} className="card">
+        <Card className="card" key={index}>
           <div className="user-information">
             {`${user.userName} (${user.userAge} years old)`}
           </div>
